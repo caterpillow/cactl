42e55621b6a4056d1cde1ccd8bd7e1cb6d2e23f1
caterpillow <skylander.alexander@gmail.com>
2026-06-04 12:36:57 +1000

    removed some stuff

--- content/strings/KMP.h
+++ content/strings/KMP.h
@@ -11,18 +11,18 @@
 #pragma once
 
 vi pi(const string& s) {
-	vi p(sz(s));
-	rep(i,1,sz(s)) {
-		int g = p[i-1];
-		while (g && s[i] != s[g]) g = p[g-1];
+	vi p(size(s));
+	FOR (i, 1, size(s)) {
+		int g = p[i - 1];
+		while (g && s[i] != s[g]) g = p[g - 1];
 		p[i] = g + (s[i] == s[g]);
 	}
 	return p;
 }
 
-vi match(const string& s, const string& pat) {
+vi match(const string &s, const string &pat) {
 	vi p = pi(pat + '\0' + s), res;
-	rep(i,sz(p)-sz(s),sz(p))
-		if (p[i] == sz(pat)) res.push_back(i - 2 * sz(pat));
+	FOR (i, size(p) - size(s), size(p))
+		if (p[i] == size(pat)) res.push_back(i - 2 * size(pat));
 	return res;
 }
